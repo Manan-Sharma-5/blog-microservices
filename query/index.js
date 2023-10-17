7b372b702a92243807794f3566e529a4f06e33ca
Manan Sharma <112090668+Manan-Sharma-5@users.noreply.github.com>
2023-10-18 02:41:59 +0530

    added docker files

--- query/index.js
+++ query/index.js
@@ -42,9 +42,13 @@ app.post("/events", (req, res) => {
 
 app.listen(4002, async () => {
   console.log("Listening on 4002");
-  const res = await axios.get("http://localhost:4005/events");
-  for (let event of res.data) {
-    console.log("Processing event:", event.type);
-    handleEvent(event.type, event.data);
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
+      handleEvent(event.type, event.data);
+    }
+  } catch (err) {
+    console.log(err.message);
   }
 });
